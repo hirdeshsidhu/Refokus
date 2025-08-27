1b69582605b91f5e3440fe380a4b7c634f6bd36b
Hirdesh Sidhu <hirdshsinghsidhu@gmail.com>
2025-08-27 15:42:24 +0530

    Added new features / bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import Work from './Components/Work'
-import Stripes from './Components/Stripes'
-import Products from './Components/Products'
-import Marquees from './Components/Marquees'
-import Cards from './Components/Cards'
-import Footer from './Components/Footer'
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Work from "./Components/Work";
+import Stripes from "./Components/Stripes";
+import Products from "./Components/Products";
+import Marquees from "./Components/Marquees";
+import Cards from "./Components/Cards";
+import Footer from "./Components/Footer";
+import LocomotiveScroll from "locomotive-scroll";
 
 function App() {
+  const scroll = new LocomotiveScroll();
   return (
     <div className='w-full h-screen bg-[#0D0D0D] text-white font-["satoshi"]'>
       <Navbar />
@@ -18,7 +20,7 @@ function App() {
       <Cards />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
